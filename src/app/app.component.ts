608199873d8cc4eb13e4a49fcf7dafd887f1495d
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-31 13:39:32 -0300

    validar existencia usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './models';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Examen-Angular-Superior';
+  user$: Observable<User | null>;
+  constructor(private readonly authenticationService: AuthenticationService) {
+    this.user$ = this.authenticationService.user$;
+  }
 }
