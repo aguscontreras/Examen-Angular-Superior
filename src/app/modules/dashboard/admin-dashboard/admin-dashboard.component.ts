2ab4131c6245072a9129e67ad77c2a75e8be5cae
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-31 13:41:03 -0300

    creado admin dash

--- src/app/modules/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../../models';
+import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
-
-  constructor() { }
+  user$: Observable<User | null>;
+  constructor(private readonly authenticationService: AuthenticationService) {}
 
   ngOnInit(): void {
+    this.user$ = this.authenticationService.user$;
   }
-
 }
