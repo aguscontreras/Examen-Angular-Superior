5101d519b35e5bc23b82bbf87130d16a10811d95
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-31 13:40:50 -0300

    componentes dash y card

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,12 +4,23 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { MaterialModule } from '../material/material.module';
+import { FormDashboardComponent } from './form-dashboard/form-dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardDashboardComponent } from './card-dashboard/card-dashboard.component';
 
 @NgModule({
   declarations: [
     AdminDashboardComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    FormDashboardComponent,
+    CardDashboardComponent,
   ],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
 export class DashboardModule {}
