72f05f22905dc9e570786a0bdf5f2c7b2d376d97
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-31 13:41:30 -0300

    user dash

--- src/app/modules/dashboard/user-dashboard/user-dashboard.component.ts
+++ src/app/modules/dashboard/user-dashboard/user-dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../../models';
+import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.scss']
+  styleUrls: ['./user-dashboard.component.scss'],
 })
 export class UserDashboardComponent implements OnInit {
-
-  constructor() { }
+  user$: Observable<User | null>;
+  constructor(private readonly authenticationService: AuthenticationService) {}
 
   ngOnInit(): void {
+    this.user$ = this.authenticationService.user$;
   }
-
 }
